327f0ba66e82909ba1c4633d25621fe2a0de88ee
Maliken <maliken.webdev@gmail.com>
2019-04-12 11:35:03 +0300

    js

--- js/plugin.js
+++ js/plugin.js
@@ -1,5 +1,24 @@
+console.log('hello world');
 
+// >> Типы данных
+// ======== Примитивные ========
+// --\
+// --| String 'Hello', "Hello"
+// --| Number 20, 8.5
+// --| Boolean true, false
+// --| Null - null
+// --| Undefined - undefined
+// --| Symbol
 
+// ========= Сложные ==========
+// --\
+// --| Object { name: 'Vadim' } 
+// --| Array [ 1, 2, 3 ]
+// --| Function
+// --| Date
 
+// >> Переменные var, let, const
+// var userName = "Vadim";
+// var userName = "Den"; // <-- проблема с var(можно еще раз объявить переменную)
+// userName = "Den"; 
 
-fs
